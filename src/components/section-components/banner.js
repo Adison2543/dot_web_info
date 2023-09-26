062699f737b688ee214254d7f8b473766088499d
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2023-09-26 13:57:08 +0700

    fx

--- src/components/section-components/banner.js
+++ src/components/section-components/banner.js
@@ -17,8 +17,8 @@ class Banner extends Component {
 			        </div>
 			      </div>
 			      <div className="col-lg-7 order-lg-1 align-self-center">
-			        <div className="banner-inner text-center text-lg-left mt-5 mt-lg-0 p-4 banner-title" >
-						<img src={publicUrl+"assets/img/banner/1.png"} alt="img" className='shape-backg'></img>
+			        <div className="fadeL banner-inner text-center text-lg-left mt-5 mt-lg-0 p-4 banner-title" >
+						{/* <img src={publicUrl+"assets/img/banner/1.png"} alt="img" className='shape-backg'></img> */}
 			          <h6 className="b-animate-1 sub-title">Ministry of Public Works and Transport</h6>
 			          <h1 className="b-animate-2 title">ກະຊວງໂຍທາທິການ ແລະ ຂົນສົ່ງ</h1>
 			          <Link className="btn btn-base b-animate-3 mr-sm-3 mr-2" to="/">Get A Quote</Link>
@@ -28,8 +28,6 @@ class Banner extends Component {
 			    </div>
 			  </div>
 			</div>
-
-
         }
 }
 
