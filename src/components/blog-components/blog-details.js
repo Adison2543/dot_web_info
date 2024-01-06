94e4a8c0095bc17635bc76abab4233fee0ad50d0
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-06 11:43:20 +0700

    Update blog-details.js

--- src/components/blog-components/blog-details.js
+++ src/components/blog-components/blog-details.js
@@ -9,6 +9,7 @@ import { Helmet } from 'react-helmet';
 import { FacebookShareButton, FacebookShareCount, TwitterShareButton, FacebookIcon, TwitterIcon, XIcon } from 'react-share';
 import { FacebookProvider, Like, Comments  } from 'react-facebook';
 
+
 const BlogDetails = () => {
 	const {t} = useTranslation();
 	const { news_id } = useParams();
@@ -34,7 +35,7 @@ const BlogDetails = () => {
 		const fetchNews = async () => {
 			try {
 				setLoading(true);
-				let fetchNews1 = await axios.get(`https://oasapi.iddriver.com/news/get/${news_id}`, 
+				let fetchNews1 = await axios.get(`https://dot-api.mpwt.gov.la/news/get/${news_id}`, 
 					{
 						headers: { 
 							'Content-Type': 'application/json', 
@@ -55,58 +56,58 @@ const BlogDetails = () => {
 
     return (
 		<div className="blog-area pd-top-120 pd-bottom-120">
-			<div className="container">
-				{loading ? <div className='w-100 d-flex justify-content-center align-items-center'><ReactLoading type='bars' color="var(--main-color)" height={200} width={100} /></div>
-				:
-				<div className="row">
-					<div className="col-lg-8">
-						<div className="blog-details-page-content">
-							<div className="single-blog-inner">
-								<div className='d-flex justify-content-end py-2 gap-2'>
-									<FacebookShareButton
-										url={currentUrl}
-										quote={news?.news_title}
-										hashtag="#DOTSmartApp"
-									>
-										<FacebookIcon size={32} round />
-									</FacebookShareButton>
-									<TwitterShareButton
-										url={currentUrl}
-										quote={news?.news_title}
-										hashtag="#DOTSmartApp"
-									>
-										<TwitterIcon size={32} round />
-									</TwitterShareButton>
-									<FacebookShareCount url={currentUrl}>
-										{(shareCount) => <span className="myShareCountWrapper">{shareCount}</span>}
-									</FacebookShareCount>
-								</div>
-								<div className="thumb"
-									style={{
-										maxHeight: '450px',
-										overflow: 'hidden'
-									}}
-								>
-									<img src={news?.news_cover ? `https://dot-api.mpwt.gov.la/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/logoPWT.png"} 
-										className='w-100' alt="img" />
-								</div>
-								<div className="details">
-									<ul className="blog-meta">
-										<li><i className="fa fa-user" /> {t('by')} {news?.user_update? news?.user_update : news?.user_create}</li>
-										<li><i className="fa fa-calendar-check-o" />{gatFDate(news?.udp_date ? news.udp_date : news.crt_date)}</li>
-									</ul>
-									<h3 className="title">{news?.news_title}</h3>
-									<p>{news?.news_description}</p>
-								</div>
-							</div>
-						</div>
+		  <div className="container">
+			{loading ? <div className='w-100 d-flex justify-content-center align-items-center'><ReactLoading type='bars' color="var(--main-color)" height={200} width={100} /></div>
+			:
+		    <div className="row">
+		      <div className="col-lg-8">
+		        <div className="blog-details-page-content">
+		          <div className="single-blog-inner">
+				  	<div className='d-flex justify-content-end py-2 gap-2'>
+						<FacebookShareButton
+							url={currentUrl}
+							quote={news?.news_title}
+							hashtag="#DOTSmartApp"
+						>
+							<FacebookIcon size={32} round />
+						</FacebookShareButton>
+						<TwitterShareButton
+							url={currentUrl}
+							quote={news?.news_title}
+							hashtag="#DOTSmartApp"
+						>
+							<TwitterIcon size={32} round />
+						</TwitterShareButton>
+						<FacebookShareCount url={currentUrl}>
+							{(shareCount) => <span className="myShareCountWrapper">{shareCount}</span>}
+						</FacebookShareCount>
 					</div>
-				<Sidebar id={news?.news_type? news.news_type : 0} tran={t}/>
-				</div>
-				}
-			</div>
+		            <div className="thumb"
+						style={{
+							maxHeight: '450px',
+							overflow: 'hidden'
+						}}
+					>
+		              <img src={news?.news_cover ? `https://dot-api.mpwt.gov.la/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/logoPWT.png"} 
+					  	className='w-100' alt="img" />
+		            </div>
+		            <div className="details">
+		              <ul className="blog-meta">
+		                <li><i className="fa fa-user" /> {t('by')} {news?.user_update? news?.user_update : news?.user_create}</li>
+		                <li><i className="fa fa-calendar-check-o" />{gatFDate(news?.udp_date ? news.udp_date : news.crt_date)}</li>
+		              </ul>
+		              <h3 className="title">{news?.news_title}</h3>
+		              <p>{news?.news_description}</p>
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		       <Sidebar id={news?.news_type? news.news_type : 0} tran={t}/>
+		    </div>
+			}
+		  </div>
 
-			{/* Add dynamic Open Graph meta tags */}
+		  {/* Add dynamic Open Graph meta tags */}
 			<Helmet>
 				<meta property="og:title" content={news?.news_title} />
 				<meta property="og:description" content={news?.news_description} />
