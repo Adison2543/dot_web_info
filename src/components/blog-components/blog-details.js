f15ce27c0c3aa4b0ed8529735c465f3f0e8f50c3
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-09 13:26:43 +0700

    addSSRByReactSnap

--- src/components/blog-components/blog-details.js
+++ src/components/blog-components/blog-details.js
@@ -55,11 +55,7 @@ const BlogDetails = () => {
     return (
 		<>
 			<Helmet prioritizeSeoTags>
-				{/* <meta property="og:title" content='DOT Smart App' />
-				<meta property="og:description" content='Department of Transport' />
-				<meta property="og:image" content={process.env.PUBLIC_URL+"assets/img/banner/b1.jpg"} />
-				<meta property="og:url" content={currentUrl} /> */}
-				<title>{news? news.news_title : 'Department of Transport (DoT)'}</title>
+				<title>{news? news.news_title : 'Unknows'}</title>
 				<meta property="og:title" content='DOT Smart App' />
 			</Helmet>
 			<div className="blog-area pd-top-120 pd-bottom-120">
