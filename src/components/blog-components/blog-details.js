8d465b0c2eccdb1dd4535b9c1e3ef5a60f3002a4
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-01-13 09:32:36 +0700

    fx

--- src/components/blog-components/blog-details.js
+++ src/components/blog-components/blog-details.js
@@ -7,7 +7,7 @@ import ReactLoading from 'react-loading';
 import { useTranslation } from 'react-i18next';
 import { FacebookShareButton, TwitterShareButton, FacebookIcon, TwitterIcon } from 'react-share';
 import MetaTags from 'react-meta-tags';
-
+import HelMet from 'react-helmet'
 const BlogDetails = () => {
 	const {t} = useTranslation();
 	const { news_id } = useParams();
@@ -61,66 +61,87 @@ const BlogDetails = () => {
 			}
 		}
 	}, [news]);	
+	return ( <HelMet>
+		<title>DOT</title>
+		<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+		<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="description" key="description" content={"Description DOT"} />
+		<meta name="title" key="title" content={"DOT"} />
+		<meta property="og:title" key="og:title" content={"DOT"} />
+		<meta property="og:locale" key="og:locale" content="en_US" />
+		<meta charSet="utf-8" />
+		<meta property="og:type" key="og:type" content="website" />
+		<meta
+		  property="og:description"
+		  key="og:description"
+		  content={"Description DOT"}
+		/>
+		<meta
+		  property="og:image"
+		  key="og:image"
+		  content={`https://th.bing.com/th/id/R.7fa5e17cefa179b2e6992ec6a1fd3c2d?rik=hlz9QVxuonnbmA&pid=ImgRaw&r=0`}
+		/>  
+	  </HelMet>)
 
-    return (
-		<>
-			<div className="blog-area pd-top-120 pd-bottom-120">
-				<MetaTags>
-					<title>Department of Transport (DoT) - {news.news_title}</title>
-					<meta name="description" content="Department of Transport (DoT)" />
-					<meta property="og:title" content={news?.news_title} />
-					<meta property="og:image" content={news?.news_cover ? `https://oasapi.iddriver.com/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/banner/b1.jpg"} />
-				</MetaTags>
-				<div className="container">
-					{loading ? <div className='w-100 d-flex justify-content-center align-items-center'><ReactLoading type='bars' color="var(--main-color)" height={200} width={100} /></div>
-					:
-					<div className="row">
-						<div className="col-lg-8">
-							<div className="blog-details-page-content">
-								<div className="single-blog-inner">
-									<div className='d-flex justify-content-end py-2 gap-2'>
-										<FacebookShareButton
-											url={currentUrl}
-											hashtag="#DOTSmartApp"
-										>
-											<FacebookIcon size={32} round />
-										</FacebookShareButton>
+    // return (
+	// 	<>
+	// 		<div className="blog-area pd-top-120 pd-bottom-120">
+	// 			<MetaTags>
+	// 				<title>Department of Transport (DoT) - {news.news_title}</title>
+	// 				<meta name="description" content="Department of Transport (DoT)" />
+	// 				<meta property="og:title" content={news?.news_title} />
+	// 				<meta property="og:image" content={news?.news_cover ? `https://oasapi.iddriver.com/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/banner/b1.jpg"} />
+	// 			</MetaTags>
+	// 			<div className="container">
+	// 				{loading ? <div className='w-100 d-flex justify-content-center align-items-center'><ReactLoading type='bars' color="var(--main-color)" height={200} width={100} /></div>
+	// 				:
+	// 				<div className="row">
+	// 					<div className="col-lg-8">
+	// 						<div className="blog-details-page-content">
+	// 							<div className="single-blog-inner">
+	// 								<div className='d-flex justify-content-end py-2 gap-2'>
+	// 									<FacebookShareButton
+	// 										url={currentUrl}
+	// 										hashtag="#DOTSmartApp"
+	// 									>
+	// 										<FacebookIcon size={32} round />
+	// 									</FacebookShareButton>
 
-										<TwitterShareButton
-											url={currentUrl}
-											quote={news?.news_title}
-											hashtag="#DOTSmartApp"
-										>
-											<TwitterIcon size={32} round />
-										</TwitterShareButton>
-									</div>
-									<div className="thumb"
-										style={{
-											maxHeight: '450px',
-											overflow: 'hidden'
-										}}
-									>
-										<img src={news?.news_cover ? `https://oasapi.iddriver.com/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/logoPWT.png"} 
-											className='w-100' alt="img" />
-									</div>
-									<div className="details">
-										<ul className="blog-meta">
-											<li><i className="fa fa-user" /> {t('by')} {news?.user_update? news?.user_update : news?.user_create}</li>
-											<li><i className="fa fa-calendar-check-o" />{gatFDate(news?.udp_date ? news.udp_date : news.crt_date)}</li>
-										</ul>
-										<h3 className="title">{news?.news_title}</h3>
-										<p>{news?.news_description}</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					<Sidebar id={news?.news_type? news.news_type : 0} tran={t}/>
-					</div>
-					}
-				</div>
-			</div>
-		</>
-    )
+	// 									<TwitterShareButton
+	// 										url={currentUrl}
+	// 										quote={news?.news_title}
+	// 										hashtag="#DOTSmartApp"
+	// 									>
+	// 										<TwitterIcon size={32} round />
+	// 									</TwitterShareButton>
+	// 								</div>
+	// 								<div className="thumb"
+	// 									style={{
+	// 										maxHeight: '450px',
+	// 										overflow: 'hidden'
+	// 									}}
+	// 								>
+	// 									<img src={news?.news_cover ? `https://oasapi.iddriver.com/media_file/file/?f=${news?.news_cover}` : process.env.PUBLIC_URL +"/assets/img/logoPWT.png"} 
+	// 										className='w-100' alt="img" />
+	// 								</div>
+	// 								<div className="details">
+	// 									<ul className="blog-meta">
+	// 										<li><i className="fa fa-user" /> {t('by')} {news?.user_update? news?.user_update : news?.user_create}</li>
+	// 										<li><i className="fa fa-calendar-check-o" />{gatFDate(news?.udp_date ? news.udp_date : news.crt_date)}</li>
+	// 									</ul>
+	// 									<h3 className="title">{news?.news_title}</h3>
+	// 									<p>{news?.news_description}</p>
+	// 								</div>
+	// 							</div>
+	// 						</div>
+	// 					</div>
+	// 				<Sidebar id={news?.news_type? news.news_type : 0} tran={t}/>
+	// 				</div>
+	// 				}
+	// 			</div>
+	// 		</div>
+	// 	</>
+    // )
   }
 
 export default BlogDetails;
