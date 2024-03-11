9f4bfcee06381dffe7214520152f39c880f5ea3d
Adison Wonglakhon <adison.wonglakhon@kkumail.com>
2024-03-11 16:23:25 +0700

    Update course-card2.js

--- src/components/section-components/course-card2.js
+++ src/components/section-components/course-card2.js
@@ -13,37 +13,39 @@ const CourseCard = ({ course, tran }) => {
   return (
     <>
       <div className="col-md-6">
-        <div className="single-course-inner">
-          <div className="thumb text-center">
-            {/* <img src={course.course_cover} alt="img" height="200px" /> */}
-            <img src={`https://dot-api.mpwt.gov.la/media_file/file/?f=${course?.course_cover}`} style={{objectFit: 'cover'}} width={370} height={250}  alt="img" />
-          </div>
-          <div className="details">
-            <div className="details-inner">
-              <div className="emt-user">
-                <i className="fa fa-user me-2" />
-                <span className="align-self-center">{course.user_update ? course.user_update : course.user_create}</span>
-              </div>
-              <h6>
-                <Link to={`/course-details/${course.course_id}/${course.course_name}`}>{course.course_name}</Link>
-              </h6>
+        <Link to={`/course-details/${course.course_id}/${course.course_name}`}>
+          <div className="single-course-inner">
+            <div className="thumb text-center">
+              {/* <img src={course.course_cover} alt="img" height="200px" /> */}
+              <img src={`https://dot-api.mpwt.gov.la/media_file/file/?f=${course?.course_cover}`} style={{objectFit: 'cover'}} width={370} height={250}  alt="img" />
             </div>
-            <div className="emt-course-meta">
-              <div className="row">
-                <div className="col-6">
-                  <div className="rating">
-                    {tran('lastupdate')}: {gatFDate(course.udp_date)}
+            <div className="details">
+              <div className="details-inner">
+                <div className="emt-user">
+                  <i className="fa fa-user me-2" />
+                  <span className="align-self-center">{course.user_update ? course.user_update : course.user_create}</span>
+                </div>
+                <h6>
+                  {course.course_name}
+                </h6>
+              </div>
+              <div className="emt-course-meta">
+                <div className="row">
+                  <div className="col-6">
+                    <div className="rating">
+                      {tran('lastupdate')}: {gatFDate(course.udp_date)}
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="price text-right">
+                      {tran('coursecode')}: <span>{course.course_code}</span>
+                    </div>
                   </div>
                 </div>
-                <div className="col-6">
-                  <div className="price text-right">
-                    {tran('coursecode')}: <span>{course.course_code}</span>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       </div>
     </>
   );
