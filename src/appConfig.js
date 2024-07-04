c4b44ee50a5744d1a055bef365ec99b419c7f13a
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-07-04 13:57:40 +0700

    Update appConfig.js

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,9 +1,21 @@
-// appConfig.js
-// UAT --> https://oasapi.iddriver.com
-// Prod --> https://dot-api.mpwt.gov.la
-export const API_BASE_URL = 'https://oasapi.iddriver.com';
+/*
+
+-------- UAT Link ---------------------
+API --> https://oasapi.iddriver.com
+Front --> https://oas.iddriver.com
+Back --> https://oasadmin.iddriver.com
+Web Info --> http://information.iddriver.com
+
+-------- Production Link --------------
+API --> https://dot-api.mpwt.gov.la
+Front --> https://dot-learning.mpwt.gov.la
+Back --> https://dot-admin.mpwt.gov.la
+Web Info --> https://dot-smart.mpwt.gov.la
+
+*/
+export const API_BASE_URL = 'https://dot-api.mpwt.gov.la';
 export const API_HEADERS = {
   'Content-Type': 'application/json',
   'Authorization': 'ZeBuphebrltl3uthIFraspubroST80Atr9tHuw5bODowi26p'
 };
-export const LINK_LEARNING = 'http://course.iddriver.com';
+export const LINK_LEARNING = 'https://dot-learning.mpwt.gov.la';
