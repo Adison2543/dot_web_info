6e75f868d21fbbd0f6feffe0c530dcec8b150b97
Adison Wonglakhon <131834926+Adisonben@users.noreply.github.com>
2024-08-13 15:26:47 +0700

    Update blog-details.js

--- src/components/blog-details.js
+++ src/components/blog-details.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './global-components/navbar-use2';
 import PageHeader from './global-components/page-header';
 import Blog from './blog-components/blog-details';
-import Footer from './global-components/footer';
+import Footer from './global-components/footer-use';
 import { useTranslation } from 'react-i18next';
 
 const BlogDetails = () => {
